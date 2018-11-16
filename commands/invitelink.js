2d8b34cf245d2396c7de6026e8fbaa513440cd6c
Cookie <asmallcookie@outlook.be>
2018-11-16 23:58:20 +0100

    Update !invitelink
    Adding a role check

--- commands/invitelink.js
+++ commands/invitelink.js
@@ -1,5 +1,24 @@
+const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
-  message.channel.send("Lien d'invitation: https://discord.gg/y4vTKAR");
+
+  //Get admin and moderator roles
+  const administratorRole = message.guild.roles.find(
+    role => role.name === "Administrator"); 
+  const moderatorRole = message.guild.roles.find(
+    role => role.name === "Moderator");
+  
+
+  // Check if message's member is moderator or administrator.
+  if (message.member.roles.has(administratorRole.id) || (message.member.roles.has(moderatorRole.id))){
+      message.channel.send("Lien d'invitation: https://discord.gg/y4vTKAR");
+  } else {
+      let errorEmbed = new Discord.RichEmbed()
+      .setColor("e74c3c")
+      .setTitle("Erreur")
+      .setDescription('Vous n\'avez pas la permission d\'effectuer cette commande.')
+      .setTimestamp('*' + new Date() + '*')
+      return message.channel.send(errorEmbed);
+  }
 };
 
 module.exports.config = {
