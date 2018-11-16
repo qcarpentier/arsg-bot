1419208c17c8a7d8dd696c906e3528631200302d
Quentin <quentin.carpentier@outlook.be>
2018-11-16 11:59:04 +0100

    Fix code format

--- commands/say.js
+++ commands/say.js
@@ -1,27 +1,27 @@
 module.exports.run = async (bot, message, args) => {
-    // Get contributore role
-    const contributorRole = message.guild.roles.find(
-        role => role.name === "Moderator"
-    );
-    const channel = message.channel;
+  // Get contributore role
+  const contributorRole = message.guild.roles.find(
+    role => role.name === "Moderator"
+  );
+  const channel = message.channel;
 
-    // Put all arguments into a string
-    let messageToSend = args.join(" ");
-    messageToSend = messageToSend.charAt(0).toUpperCase() + messageToSend.slice(1);
-  
-    // Delete the command message
-    message.delete();
+  // Put all arguments into a string
+  let messageToSend = args.join(" ");
+  messageToSend = messageToSend.charAt(0).toUpperCase() + messageToSend.slice(1);
 
-    // Verify if sender has contributor role
-    if (message.member.roles.has(contributorRole.id)) {
-        // Send the message
-        channel.send(messageToSend);
-    } else {
-        // Send wrong permission message
-        channel.send("Vous n'avez pas le droit d'utiliser cette commande.");
-    }
+  // Delete the command message
+  message.delete();
+
+  // Verify if sender has contributor role
+  if (message.member.roles.has(contributorRole.id)) {
+    // Send the message
+    channel.send(messageToSend);
+  } else {
+    // Send wrong permission message
+    channel.send("Vous n'avez pas le droit d'utiliser cette commande.");
+  }
 };
-  
+
 module.exports.config = {
-    command: "say"
+  command: "say"
 };
--- commands/setactivity.js
+++ commands/setactivity.js
@@ -1,42 +1,41 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
-            if(args.length <= 1){
-                let errorembed = new Discord.RichEmbed()
-                        .setColor("e74c3c") // Generate random color
-                        .setTitle("~ Erreur, paramêtres manquants")
-                        .setDescription('Vous devez spécifier **deux** paramètre pour la commande **!setactivity**')
-                        .setTimestamp(new Date()) //Get date
-                        .setFooter(`Exception levée par ${message.author.tag}`);
-                        return message.channel.send(errorembed);
-                }
-                else 
-                { // Type possible : Streaming, Listening, Watching, Playing
-                        if(args[1].length === 0){args[1] = "playing"} //Exception fix (Cannot read property 'length' of undefined)
-                        args[1] = args[1].toLowerCase()
-                        if(args[1] === "playing" || args[1] === "streaming" || args[1] === "listening" || args[1] === "watching"){
-                            let activityembed = new Discord.RichEmbed()
-                            .setColor("0x0080ff") // Generate random color
-                            .setTitle("~ Changement de l\'activité du bot")
-                            .setDescription('Bot activity **=>** ' + args[0] + '; Type => ' + '**' + args[1].toUpperCase() + '**')
-                            .setTimestamp(new Date()) //Get date
-                            .setFooter(`Demandé par ${message.author.tag}`);
-                            let typeactivity = args[1].toUpperCase()
-                            bot.user.setActivity(args, { type: typeactivity });
-                            return message.channel.send(activityembed);
-                    } 
-                    else 
-                    {
-                            let activityembed = new Discord.RichEmbed()
-                            .setColor("0x0080ff") // Generate random color
-                            .setTitle("~ Changement de l\'activité du bot")
-                            .setDescription('Bot activity **=>** ' + args[0] + ' ; Type => **PLAYING**' )
-                            .setTimestamp(new Date()) //Get date
-                            .setFooter(`Demandé par ${message.author.tag}`);
-                            let typeactivity = "PLAYING"
-                            bot.user.setActivity(args, { type: typeactivity });
-                            return message.channel.send(activityembed);
-                    }
+  if (args.length <= 1) {
+    let errorembed = new Discord.RichEmbed()
+      .setColor("e74c3c") // Generate random color
+      .setTitle("~ Erreur, paramêtres manquants")
+      .setDescription('Vous devez spécifier **deux** paramètre pour la commande **!setactivity**')
+      .setTimestamp(new Date()) //Get date
+      .setFooter(`Exception levée par ${message.author.tag}`);
+    return message.channel.send(errorembed);
+  }
+  else
+  { // Type possible : Streaming, Listening, Watching, Playing
+    if (args[1].length === 0) { args[1] = "playing" } //Exception fix (Cannot read property 'length' of undefined)
+    args[1] = args[1].toLowerCase()
+    if (args[1] === "playing" || args[1] === "streaming" || args[1] === "listening" || args[1] === "watching") {
+      let activityembed = new Discord.RichEmbed()
+        .setColor("0x0080ff") // Generate random color
+        .setTitle("~ Changement de l\'activité du bot")
+        .setDescription('Bot activity **=>** ' + args[0] + '; Type => ' + '**' + args[1].toUpperCase() + '**')
+        .setTimestamp(new Date()) //Get date
+        .setFooter(`Demandé par ${message.author.tag}`);
+      let typeactivity = args[1].toUpperCase()
+      bot.user.setActivity(args, { type: typeactivity });
+      return message.channel.send(activityembed);
     }
+    else {
+      let activityembed = new Discord.RichEmbed()
+        .setColor("0x0080ff") // Generate random color
+        .setTitle("~ Changement de l\'activité du bot")
+        .setDescription('Bot activity **=>** ' + args[0] + ' ; Type => **PLAYING**')
+        .setTimestamp(new Date()) //Get date
+        .setFooter(`Demandé par ${message.author.tag}`);
+      let typeactivity = "PLAYING"
+      bot.user.setActivity(args, { type: typeactivity });
+      return message.channel.send(activityembed);
+    }
+  }
 };
 
 module.exports.config = {
