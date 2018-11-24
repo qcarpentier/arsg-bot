023b1f9887ed2c8e1533b9f8ff5c81253156930b
Quentin Carpentier <quentin.carpentier@outlook.be>
2018-11-24 09:41:54 +0100

    Add !say, !setactivity and !mute commands. Also, rework !invitelink and !ping commands as well. (#11)
    * Command handler: read the commands folder, set the modules in the command file, and run the command
    
    * Ping and InviteLink moved to /commands
    
    * Google and GoogleImg moved to /commands
    
    * Rules and Markdown moved to /commands
    
    * Help and SetHomework moved to /commands
    
    * Unpin and Purge moved to /commands
    
    * Prettier + ESLint rework :)
    
    * Comment logs
    
    * Add say command
    
    * Update commands/ping.js
    
    New !ping commands
    
    * Add commands/setactivity.js
    
    New commands (!activity arg0 arg1)
    Set activity of the bot
    
    * Update commands/ping.js
    
    * Fix code format
    
    * Update !invitelink
    
    Adding a role check
    
    * !ping command rework: call the real API ping and the actual bot ping
    
    * Add !mute command v1

--- commands/ping.js
+++ commands/ping.js
@@ -1,5 +1,20 @@
+const Discord = require("discord.js");
+
 module.exports.run = async (bot, message, args) => {
-  message.channel.send("pong!");
+  const startTime = new Date();
+  const member = message.member.displayName;
+
+  message.channel.send("pong!").then(message => {
+    const endTime = new Date();
+    const apiPing = Math.round(endTime - startTime);
+
+    let pingEmbed = new Discord.RichEmbed()
+      .setColor("RANDOM") // Generate random color 
+      .addField('API Ping : ', apiPing + ' ms') // Calculate ms ping of API call
+      .addField('Bot Ping : ', bot.ping + ' ms') // Call the actual bot ping
+      .setFooter(`Demandé par ${member}`);
+    return message.channel.send(pingEmbed);
+  })
 };
 
 module.exports.config = {
