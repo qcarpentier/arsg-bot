945b78b88cd0a0c4abd0b398ab28710d2299fa6f
Quentin Carpentier <quentin.carpentier@outlook.be>
2018-11-30 16:53:49 +0100

    Fixes: OnHold role when removing a reaction and google/googleimg

--- commands/google.js
+++ commands/google.js
@@ -1,6 +1,9 @@
 module.exports.run = async (bot, message, args) => {
   // Check if the command contains argument(s) > '!google args'
-  if (message.indexOf(" ") >= 0) {
+
+  console.log(args.length);
+  
+  if (args.length > 0) {
     // Send the Google search with all the args joined with %20 (hexcode for space)
     message.channel.send("https://www.google.be/#q=" + args.join("%20"));
   } else {
