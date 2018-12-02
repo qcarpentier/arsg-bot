9000a0d65b4196a4cedf1063e7498e4ac0019dda
Quentin <quentin.carpentier@outlook.be>
2018-12-02 22:12:30 +0100

    Fix !say command: administrator is allowed to use it and error is thrown in DM

--- commands/say.js
+++ commands/say.js
@@ -1,24 +1,32 @@
 module.exports.run = async (bot, message, args) => {
-  // Get contributore role
-  const contributorRole = message.guild.roles.find(
+  // Get administrator role
+  const administratorRole = message.guild.roles.find(
+    role => role.name === "Administrator"
+  );
+  // Get moderator role
+  const moderatorRole = message.guild.roles.find(
     role => role.name === "Moderator"
   );
   const channel = message.channel;
 
   // Put all arguments into a string
   let messageToSend = args.join(" ");
-  messageToSend = messageToSend.charAt(0).toUpperCase() + messageToSend.slice(1);
+  messageToSend =
+    messageToSend.charAt(0).toUpperCase() + messageToSend.slice(1);
 
   // Delete the command message
   message.delete();
 
   // Verify if sender has contributor role
-  if (message.member.roles.has(contributorRole.id)) {
+  if (
+    message.member.roles.has(administratorRole.id) ||
+    message.member.roles.has(moderatorRole.id)
+  ) {
     // Send the message
     channel.send(messageToSend);
   } else {
     // Send wrong permission message
-    channel.send("Vous n'avez pas le droit d'utiliser cette commande.");
+    message.author.send("Vous n'avez pas le droit d'utiliser cette commande.");
   }
 };
 
