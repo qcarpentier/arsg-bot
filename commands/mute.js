ae48109468d9180c074c1c7bfc732aa9a2cbe0e0
Quentin <quentin.carpentier@outlook.be>
2018-12-02 17:21:34 +0100

    Add a temporary argument to the mute command

--- commands/mute.js
+++ commands/mute.js
@@ -10,7 +10,6 @@ module.exports.run = async (bot, message, args) => {
   const moderatorRole = message.guild.roles.find(
     role => role.name === "Moderator"
   );
-
   // Set an Error RichEmbed
   const setError = message => {
     const memberName = member.displayName;
@@ -23,6 +22,7 @@ module.exports.run = async (bot, message, args) => {
   };
   const member = message.member;
   const author = message.author;
+  const hourRegex = /^\d+(\.\d+)?$/;
   // Get the member to be muted (actually, the first mentioned member)
   const memberToBeMuted = message.mentions.members.first();
 
@@ -43,13 +43,29 @@ module.exports.run = async (bot, message, args) => {
       "vous devez mentionner le **nom de l'élève** pour la commande `!mute`."
     );
     return author.send(errorEmbed);
+  } else if (args < 2 || !hourRegex.test(args[1])) {
+    const errorEmbed = setError(
+      "vous déterminer la période **en heure** pour la commande `!mute`."
+    );
+    return author.send(errorEmbed);
   } else {
     // Get Muted role
     const mutedRole = message.guild.roles.find(role => role.name === "Muted");
 
     // Set Muted role
-    if (!memberToBeMuted.roles.has(mutedRole.id))
+    if (!memberToBeMuted.roles.has(mutedRole.id)) {
       memberToBeMuted.addRole(mutedRole.id);
+      message.channel.send(
+        `${memberToBeMuted} vient d'être rendu au silence pour une durée de **${
+          args[1]
+        } heure(s).**`
+      );
+    }
+
+    setTimeout(() => {
+      if (memberToBeMuted.roles.has(mutedRole.id))
+        memberToBeMuted.removeRole(mutedRole.id);
+    }, args[1] * 60 * 60 * 1000);
   }
 };
 
