1c29c656a84f049cd032d8c8c0fcaf8d873b4f5a
Quentin Carpentier <quentin.carpentier@outlook.be>
2019-09-14 15:17:38 +0200

    Classement fixed

--- commands/classement.js
+++ commands/classement.js
@@ -1,65 +1,61 @@
 const Discord = require("discord.js");
+let User = require("../models/user.model");
 
 module.exports.run = async (bot, message, args) => {
-  console.log("!classement");
-  
-  // const channel = message.channel;
+  const channel = message.channel;
 
-  // // Current date format
-  // const currentDate = new Date();
-  // let day = currentDate.getDate();
-  // let month = currentDate.getMonth() + 1;
-  // const year = currentDate.getFullYear();
-  // if (day < 10) day = "0" + day;
-  // if (month < 10) month = "0" + month;
-  // // 'dd/mm/yyyy' date format
-  // const currentDateFormat = day + "/" + month + "/" + year;
+  // Current date format
+  const currentDate = new Date();
+  let day = currentDate.getDate();
+  let month = currentDate.getMonth() + 1;
+  const year = currentDate.getFullYear();
+  if (day < 10) day = "0" + day;
+  if (month < 10) month = "0" + month;
+  // 'dd/mm/yyyy' date format
+  const currentDateFormat = day + "/" + month + "/" + year;
 
-  // // Current time format
-  // const currentTime = new Date();
-  // let hour = currentDate.getHours();
-  // let minute = currentDate.getMinutes();
-  // if (hour < 10) hour = "0" + hour;
-  // if (minute < 10) minute = "0" + minute;
-  // // 'hh/mm' date format
-  // const currentTimeFormat = hour + ":" + minute;
+  // Current time format
+  let hour = currentDate.getHours();
+  let minute = currentDate.getMinutes();
+  if (hour < 10) hour = "0" + hour;
+  if (minute < 10) minute = "0" + minute;
+  // 'hh/mm' date format
+  const currentTimeFormat = hour + ":" + minute;
 
-  // const mysql = require("mysql");
+  // Get level ordered by highest > lowest
+  User.find()
+    .then(users => {
+      users.slice(0, 3).reverse();
 
-  // const connection = mysql.createConnection({
-  //   host: process.env.HOST,
-  //   user: process.env.USER,
-  //   password: process.env.PASSWORD,
-  //   database: process.env.DB
-  // });
+      const firstUserName = users[0]
+        ? users[0].username
+        : "Utilisateur non existant";
+      const firstUserLevel = users[0] ? users[0].level : 0;
+      const firstUserMessages = users[0] ? users[0].messages : 0;
+      const secondUserName = users[1]
+        ? users[1].username
+        : "Utilisateur non existant";
+      const secondUserLevel = users[1] ? users[1].level : 0;
+      const secondUserMessages = users[1] ? users[1].messages : 0;
+      const thirdUserName = users[2]
+        ? users[2].username
+        : "Utilisateur non existant";
+      const thirdUserLevel = users[2] ? users[2].level : 0;
+      const thirdUserMessages = users[2] ? users[2].messages : 0;
 
-  // // Connect to DB
-  // connection.connect(err => {
-  //   if (err) {
-  //     console.log("DB Error".red);
-  //   }
-  // });
+      const classementEmbed = new Discord.RichEmbed()
+        .setTitle(`Classement du ${currentDateFormat} à ${currentTimeFormat}`)
+        .setColor("RANDOM")
+        .setDescription(
+          `**[1]** ${firstUserName} - Level ${firstUserLevel} \n *${firstUserMessages} message(s)* \n
+           **[2]** ${secondUserName} - Level ${secondUserLevel} \n *${secondUserMessages} message(s)* \n
+           **[3]** ${thirdUserName} - Level ${thirdUserLevel} \n *${thirdUserMessages} message(s)*`
+        )
+        .setFooter(`Demandé par ${message.member.displayName}`);
 
-  // // Get level ordered by highest > lowest
-  // connection.query(
-  //   `SELECT * FROM users ORDER BY level DESC`,
-  //   (err, rows, fields) => {
-  //     if (err) throw err;
-
-  //     // Build the classement Rich Embed
-  //     const classementEmbed = new Discord.RichEmbed()
-  //       .setTitle(`Classement du ${currentDateFormat} à ${currentTimeFormat}`)
-  //       .setColor("#79B1F2")
-  //       .setDescription(
-  //         `**[1]** ${rows[0].alias} level ${rows[0].level} \n *${rows[0].message} message(s)* \n
-  //      **[2]** ${rows[1].alias} level ${rows[1].level} \n *${rows[1].message} message(s)* \n
-  //      **[3]** ${rows[2].alias} level ${rows[2].level} \n *${rows[2].message} message(s)*`
-  //       )
-  //       .setFooter(`Demandé par ${message.member.displayName}`);
-
-  //     channel.send(classementEmbed);
-  //   }
-  // );
+      channel.send(classementEmbed);
+    })
+    .catch(error => console.log(`Error: ${error}`));
 };
 
 module.exports.config = {
