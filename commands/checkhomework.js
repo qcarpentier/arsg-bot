7d603b417f518cd11b0eac13e306b557e375d527
Maxence-3D <46704939+Maxence-3D@users.noreply.github.com>
2019-10-08 18:59:08 +0200

    changing color when homework is expired

--- commands/checkhomework.js
+++ commands/checkhomework.js
@@ -1,55 +1,85 @@
 module.exports.run = async (bot, message, args) => {
+  // Exit if the command is not inside a #homework channel
+  if (!message.channel.name.includes("homework")) {
+    return message.channel.send(
+      "La commande `!sethomework` est uniquement disponible dans le channel `#homework` de votre classe."
+    );
+  }
 
-    // Homework month
-    const monthNames = [
-        "Janvier",
-        "Février",
-        "Mars",
-        "Avril",
-        "Mai",
-        "Juin",
-        "Juillet",
-        "Août",
-        "Septembre",
-        "Octobre",
-        "Novembre",
-        "Décembre"
-      ];
+  // Homework month
+  const monthNames = [
+    "Janvier",
+    "Février",
+    "Mars",
+    "Avril",
+    "Mai",
+    "Juin",
+    "Juillet",
+    "Août",
+    "Septembre",
+    "Octobre",
+    "Novembre",
+    "Décembre"
+  ];
 
-    //Homework channels
-    const channelNames = [
-        "3ttri-homework",
-        "4ttri-homework",
-        "5ttri-homework",
-        "6ttri-homework"
-    ]
+  const channelNames = [
+    "3ttri-homework",
+    "4ttri-homework",
+    "5ttri-homework",
+    "6ttri-homework"
+  ];
 
-    // for(let i in channelNames){              uncomment for use in public version
-        let testChannel = bot.channels.find(channel => channel.name === "bot-homework")         //comment for use in public version
-        // let testChannel = bot.channels.find(channel => channel.name === channelNames[i])         uncomment for use in public version
-        let today = new Date()
-        message.delete()
-        testChannel.fetchMessages().then((result) => {
-          result.forEach(msg => {
-            msg.embeds.forEach(emb => {
-                let contentSpliter = (emb.description).split(" ")
-                let content = {
-                  day: parseInt(contentSpliter[2]) + 1,
-                  month: monthNames.indexOf(contentSpliter[3]),
-                  year: parseInt(((contentSpliter[4]).split('\n'))[0])
-              }
-                if(today >= new Date(content.year, content.month, content.day+1)){
-                  msg.delete()
-                } 
-            })
+  // Check for homeworks for all channels
+  for (const channelName of channelNames) {
+    const channel = bot.channels.find(
+      chan => chan.name === channelName
+    );
+
+    channel
+      .fetchMessages()
+      .then(channelMessages =>
+        channelMessages.forEach(msg => {
+          msg.embeds.forEach(embed => {
+            const today = new Date();
+
+            // Fetch and build the homework date
+            const contentSpliter = embed.description.split(" ");
+            const content = {
+              day: parseInt(contentSpliter[2]),
+              month: parseInt(monthNames.indexOf(contentSpliter[3]) + 1),
+              year: parseInt(contentSpliter[4])
+            };
+
+            const homeworkDate = new Date(
+              content.year,
+              content.month - 1,
+              content.day + 1
+            );
+
+            console.log(homeworkDate);
+
+            if (today >= homeworkDate) {
+              msg.edit("", {
+                embed: {
+                  title: emb.title,
+                  description: emb.description,
+                  color: "3394611",
+                  footer: {
+                    text: emb.footer.text
+                  }
+                }
+              })
+            }
           });
-        })    
-        .catch(err => {
-          console.log(err)
-        })  
-    // }
+        })
+      )
+      .catch(err => console.log(err.red));
+  }
+
+  // Delete the command message
+  message.delete();
 };
-  
+
 module.exports.config = {
-    command: "checkhomework"
-};  
+  command: "checkhomework"
+};
